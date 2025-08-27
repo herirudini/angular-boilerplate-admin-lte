9a3a5b7fe1a3ffbd8bfd3e82f8cd6371931db34e
herirudini <herirudini@gmail.com>
2025-08-27 17:12:51 +0700

    fix(main-layout): sidebar-brand

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MLayFooter } from './components/m-lay-footer/m-lay-footer';
 import { MLayMain } from './components/m-lay-main/m-lay-main';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-main-layout',
-  imports: [MLayFooter, MLayMain],
+  imports: [MLayFooter, MLayMain, RouterLink],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
 })
